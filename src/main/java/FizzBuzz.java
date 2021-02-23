b8a0237d198cd578b8a413ef386ed3b0ae31a22e
Crile, Adam <Crile.Adam@principal.com>
2021-02-23 15:37:11 -0600

    Week 2, Part Two, Test 5.  Added else to return Buzz for modulus of five.

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,8 +2,10 @@
 public class FizzBuzz {
     public String execute(Integer number) {
         String result = number.toString();
-        if(number % 3 == 0){
+        if (number % 3 == 0) {
             result = "Fizz";
+        } else if (number % 5 == 0) {
+            result = "Buzz";
         }
         return result;
     }
